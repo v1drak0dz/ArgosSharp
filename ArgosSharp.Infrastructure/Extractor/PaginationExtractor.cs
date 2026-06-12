344af35328826bf78821590428ef2aae0c8e6bef
vfreire <vicente.freire@capgemini.com>
2026-06-12 13:36:34 -0300

    (refact) new scraper standard

---
+++ ArgosSharp.Infrastructure/Extractor/PaginationExtractor.cs
@@ -0,0 +1,20 @@
+﻿using AngleSharp.Browser;
+using ArgosSharp.Application.Interfaces.Parser;
+using Microsoft.Extensions.Logging;
+
+namespace ArgosSharp.Infrastructure.Utils
+{
+    public static class PaginationExtractor
+    {
+        public static int GetMaxPage(IEnumerable<string>? pagination)
+        {
+            var numbers = pagination?
+                .Select(x => int.TryParse(x.Trim(), out var n) ? n : (int?)null)
+                .Where(x => x.HasValue)
+                .Select(x => x!.Value)
+                .ToList();
+
+            return numbers!.Any() ? numbers!.Max() : 1;
+        }
+    }
+}
